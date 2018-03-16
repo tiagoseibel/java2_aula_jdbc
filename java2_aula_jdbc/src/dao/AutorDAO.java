9a4edb22db829537a4de1f4749a1ad1ec330c03e
tiagoseibel <tiagoseibel@gmail.com>
2018-03-15 21:03:17 -0300

    Estrutura DAO

---
+++ java2_aula_jdbc/src/dao/AutorDAO.java
@@ -0,0 +1,24 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Autor;
+
+public class AutorDAO {
+    public void save(Autor autor) {  
+    }
+    
+    public void update(Autor autor) {
+    }
+    
+    public void delete(Autor autor) {
+    }
+    
+    public Autor findById(int id) {
+        return new Autor();
+    }
+    
+    public List<Autor> findAll() {
+        return new ArrayList<>();
+    }
+}
---
+++ java2_aula_jdbc/src/model/Autor.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Autor {
+    private int autor_id;
+    private String nome;
+
+    public int getAutor_id() {
+        return autor_id;
+    }
+
+    public void setAutor_id(int autor_id) {
+        this.autor_id = autor_id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
